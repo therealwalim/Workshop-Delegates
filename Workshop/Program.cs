e3fc7fa0f4f7ef84fcc8bed543934c3d9b2185b6
Walim ECHAIB <walim.echaib.dz@viacesi.fr>
2019-11-24 11:50:06 +0100

    Ajoutez des fichiers projet.

---
+++ Workshop/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Workshop
+{
+    class Program
+    {
+        // Workshop QST 1
+
+        //delegate void MyDelegate(int v1,int v2);
+
+        //static void Adt(int v1, int v2)
+        //{
+        //    int r1 = v2 + v1;
+        //    Console.WriteLine("Le resultat est : {0} ", r1);
+        //}
+
+        // Workshop QST 2
+        //delegate int DelegateType(int i);
+
+        // Workshop QST 3
+        static void Main(string[] args)
+        {
+            // QST 1
+            //MyDelegate adl = Adt;
+            //adl(1,2);
+
+            // QST 2
+            //DelegateType myDelegate = x => x*x;
+            //int square = myDelegate(2);
+
+            //Console.WriteLine(square);
+
+            // QST 3
+            //var v = new { Age = 20, Name = "Walim" };
+
+            //Console.WriteLine("My name is {0} and my age is {1}", v.Name, v.Age);
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
